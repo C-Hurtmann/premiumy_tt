446fe080499d28204d04152f7f63b0af9659881f
Constantine Zagorodnyi <constantine2903@gmail.com>
2025-03-11 21:24:04 +0300

    finish captcha solver

--- email_manager.py
+++ email_manager.py
@@ -1,10 +1,6 @@
 import hashlib
 from pprint import pprint
-from faker import Faker
 import requests
-
-
-fake = Faker()
 
 
 class EmailManager:
@@ -13,21 +9,23 @@ class EmailManager:
         'x-rapidapi-host': 'privatix-temp-mail-v1.p.rapidapi.com',
         'x-rapidapi-key': 'e5d8f94a11msh0e0b78259340742p11851djsne46a022e76aa'
     }
-    current_email_address: str
 
+    def __init__(self, first_name: str, last_name: str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+
+        self.email_address: str
+        
     @classmethod
     def _get(cls, url: str) -> requests.Response:
         return requests.get(url, headers=cls.headers)
-
         
-    @classmethod
-    def create_email_address(cls) -> str:
-        res = cls._get(cls.base_url + 'domains/')
-        email = f'{fake.last_name().lower()}.{fake.first_name().lower()[0]}' + res.json()[0]
-        cls.current_email = email
+    def create_email_address(self) -> str:
+        res = self._get(self.base_url + 'domains/')
+        email = f'{self.last_name.lower()}.{self.first_name.lower()[0]}' + res.json()[0]
+        self.email_address = email
         return email
     
-    @classmethod
-    def get_last_emails(cls):
-        res = cls._get(cls.base_url +  'mail/id/' + hashlib.md5(cls.current_email_address.encode()).hexdigest())
+    def get_last_emails(self):
+        res = self._get(self.base_url +  'mail/id/' + hashlib.md5(self.current_email_address.encode()).hexdigest())
         pprint(res.json())
--- test_selenium.py
+++ test_selenium.py
@@ -1,24 +1,31 @@
-import random
-import requests
-import hashlib
-from pprint import pprint
-from pathlib import Path
 import time
-
 from faker import Faker
-
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from lxml import etree
+from chromedriver_autoinstaller import install
+from email_manager import EmailManager
+from extention import proxies
+from twocaptcha_extension_python import TwoCaptcha
 
 
 fake = Faker()
+
+first_name = fake.first_name()
+last_name = fake.last_name()
+
+email_manager = EmailManager(first_name=first_name, last_name=last_name)
+email = email_manager.create_email_address()
+
+username = 'IKUmhvPAcfZzlVIN'
+password = 'mobile;bg;;;'
+endpoint = 'proxy.soax.com'
+port = '9000'
+website = 'https://www.valr.com/en/signup'
 
 
 def save_page_source(source: str) -> None:
@@ -29,57 +36,46 @@ def save_page_source(source: str) -> None:
         tree.write(f, pretty_print=True, encoding='utf-8', xml_declaration=True)
 
 
-with open(Path('proxyscrape_premium_http_proxies.txt')) as f:
-    proxy = random.choice(f.readlines())
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(TwoCaptcha("806126af0f402f205c1e49ffc88865f2").load())
 
+proxies_extension = proxies(username, password, endpoint, port)
+
+chrome_options.add_extension(proxies_extension)
+
+chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+wait = WebDriverWait(chrome, 20)
 
 try:
-    # Set up Chrome options
-    options = Options()
-    options.add_argument(f'--proxy-server=http://{proxy}')
+    print('Step 1: Get signup page')
+    chrome.get(website)
 
-    # Set up WebDriver with ChromeDriverManager
-    print("Setting up driver...")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    print('Step 2: find input fields')
+    first_name_field = wait.until(EC.presence_of_element_located((By.NAME, "firstName")))
+    last_name_field = wait.until(EC.presence_of_element_located((By.NAME, "lastName")))
+    email_address_field = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+    password_field = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+
+    print('Step 3: fill input fields')
+    first_name_field.send_keys(first_name)
+    last_name_field.send_keys(last_name)
+    email_address_field.send_keys(email)
+    password_field.send_keys('G$9vL2@xM!b3#Z7q')
+    chrome.execute_script("window.scrollBy(0, 500);")
+
+    print('Step 4: Resolve capcha')
+    captcha_solver = WebDriverWait(chrome, 30).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, ".captcha-solver_inner"))
+    )
+    captcha_solver.click()
 
 
-    # Main workflow
-    print("Get welcome page...")
-    driver.get('https://2ip.me/en/')
-    wait = WebDriverWait(driver, 30)
-    action = ActionChains(driver)
-
-    print('Click signup button...')
-    signup_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//a[@data-testid="header-get-started-button"]')))
-    time.sleep(6)
-    action.move_to_element(signup_button).perform()
-    signup_button.click()
-
-    print('Create email...')
-    email = EmailManager.create_email_address()
-
-    print('Fill input field...')
-    email_input_field = wait.until(EC.visibility_of_element_located((By.TAG_NAME, 'input')))
-    action.move_to_element(email_input_field).perform()
-    email_input_field.click()
-    for char in email:
-        time.sleep(random.uniform(0.1, 0.9))
-        email_input_field.send_keys(char)
-
-    print('Click submit button...')
-    continue_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-    time.sleep(3)
-    action.move_to_element(continue_button).perform()
-    continue_button.click()
-
-    print('Wait until email received...')
-    attempt = 0
-    while attempt < 1:
-        attempt += 1
-        time.sleep(10)
-        EmailManager.get_last_emails()
+    print('Step 5: Click submit')
+    WebDriverWait(chrome, 160).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+    ).click()
 
 finally:
-    time.sleep(30)
-    save_page_source(source=driver.page_source)
-    driver.quit()
+    time.sleep(50)
+    save_page_source(chrome.page_source)
+    chrome.quit()
